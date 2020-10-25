b16fa351ce19d29ec6f912762454bacd550e54e4
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-10-25 11:57:21 +0900

    Revert "room_usersの作成"
    This reverts commit 54f7b91692d67dd1058449e27caf3b6ee7590dcb.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 <<<<<<< HEAD
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_10_24_101119) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -22,9 +21,6 @@ ActiveRecord::Schema.define(version: 2020_10_24_101119) do
     t.index ["room_id"], name: "index_room_users_on_room_id"
     t.index ["user_id"], name: "index_room_users_on_user_id"
   end
-=======
-ActiveRecord::Schema.define(version: 2020_10_24_094149) do
->>>>>>> parent of 79a7734... room_usersテーブルの作成
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,4 +44,6 @@ ActiveRecord::Schema.define(version: 2020_10_24_012035) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
